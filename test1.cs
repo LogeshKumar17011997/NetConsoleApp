5df2b6daeb45d4b72cd9dc7c70aa42c6eabf6fca
Logesh <logeshkumarac14uit028@gmail.com>
2023-07-22 15:33:10 +0530

    'ignorefile'

---
+++ test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUnderstandings
+{
+	public class test1
+	{
+		public string Name { get; set; }
+	}
+}
